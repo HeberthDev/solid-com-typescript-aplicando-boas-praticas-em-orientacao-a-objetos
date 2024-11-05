5ad59f1aa3946a6b2f52ceba6365efd9e6299db4
HeberthDev <heberthdeveloper@gmail.com>
2024-11-05 07:43:20 -0300

    Dividindo as responsabilidades em classes

--- tarefa-1/Colaborador.ts
+++ tarefa-1/Colaborador.ts
@@ -1,6 +1,9 @@
 import { Cargos } from "./enum/cargos";
 
 export default class Colaborador {
+    map(arg0: (colaborador: { nome: any; cargo: Cargos; }) => { nome: any; cargo: Cargos; salario: number; }) {
+        throw new Error("Method not implemented.");
+    }
 
     public readonly nome: string;
     private _cargo: Cargos;
